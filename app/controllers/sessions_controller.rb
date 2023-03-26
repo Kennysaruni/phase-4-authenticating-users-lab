6ca57683d06afd347e7ede2191f158fea5483440
Kennysaruni <kenny.saruni@student.moringaschool.com>
2023-03-26 19:17:48 +0300

    Lab Complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
+    end
+
+    def show
+        user = User.find_by(username: params[:username])
+        if user
+            session[:user_id] = user.id
+            render json: user
+          else
+            render json: { error: "Invalid username" }, status: :unauthorized
+          end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
